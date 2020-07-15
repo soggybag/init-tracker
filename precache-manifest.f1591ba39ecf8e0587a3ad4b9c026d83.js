2e03c93e768447b378fd258ef6eaf1d5bbc2ac79
Mitchell <soggybag@gmail.com>
2020-07-15 14:32:22 -0700

    Updates

---
+++ precache-manifest.f1591ba39ecf8e0587a3ad4b9c026d83.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccd3309579857b2a88ca9c0be3e71dcd",
+    "url": "/init-tracker/index.html"
+  },
+  {
+    "revision": "f568f807620297e5e197",
+    "url": "/init-tracker/static/css/main.de7bf15f.chunk.css"
+  },
+  {
+    "revision": "4eaaf62e56d7165b1a37",
+    "url": "/init-tracker/static/js/2.042f2fac.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/init-tracker/static/js/2.042f2fac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f568f807620297e5e197",
+    "url": "/init-tracker/static/js/main.c75c153f.chunk.js"
+  },
+  {
+    "revision": "5bec1a1c3a32acd0c146",
+    "url": "/init-tracker/static/js/runtime-main.49f4da32.js"
+  }
+]);
